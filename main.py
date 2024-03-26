f9fcf328660cbe445e6f5883e3df6375fd5e9029
futurewitness <tleong073@gmail.com>
2024-03-26 19:04:03 -0400

    Updates

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def serve():
-    return "<html><b>ANDREW MONTGOMERY LEE</b></html>"
+    return render_template("index.html")
 
 
 
@@ -13,4 +14,4 @@ def hello_world():
     return jsonify(message="Hello, world!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False)
+    app.run(host='0.0.0.0')
